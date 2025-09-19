ec94e9ca0ad8e80cb896a5308f6961bd06868c26
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-09-20 00:24:45 +0530

    getProducts api

---
+++ backend/routes/product.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { getProducts } from "../controllers/product.controller.js";
+
+const router = express.Router();
+
+router.get("/",getProducts);
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,17 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 dotenv.config();
 
+import productRoutes from "./routes/product.routes.js";
+
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req,res) => {
     res.send('server is ready')
 })
+
+app.use("/api/products", productRoutes)
 
 app.listen(3000, () => {
     connectDB();

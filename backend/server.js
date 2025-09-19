ff04a80268faac258b5d6a5c4d1bab4fa6719cda
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-09-19 23:30:05 +0530

    server setup db connect

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+dotenv.config();
 
 const app = express();
 
@@ -7,5 +10,6 @@ app.get("/", (req,res) => {
 })
 
 app.listen(3000, () => {
+    connectDB();
     console.log(`Server is started`);
 })

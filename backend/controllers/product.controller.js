fc8d4b181cb087da6a525b3746af838f75f7729a
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-09-20 00:43:26 +0530

    createProduct api

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -14,3 +14,26 @@ export const getProducts = async (req,res) => {
         res.status(500).json({success : false, message : `Server Error`});
     }
 }
+
+export const createProduct = async (req,res) => {
+    try {
+        const {name,price,image} = req.body;
+
+        if(!name.length || !price.length || !image.length) {
+            return res.status(400).json({success:false, message:"all fields are required"});
+        }
+
+        const product = new Product({
+            name,
+            price,
+            image
+        });
+
+        await product.save();
+
+        res.status(201).json({success:true, message : "product is created", product});
+    } catch (error) {
+        console.log(`Error in creating the product ${error.message}`);
+        res.status(500).json({success:false, message:`internal server error`});
+    }
+}
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getProducts } from "../controllers/product.controller.js";
+import { getProducts, createProduct } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 router.get("/",getProducts);
+router.post("/createProduct", createProduct);
 
 
 export default router;

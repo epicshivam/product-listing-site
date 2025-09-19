ffacaffe03d4adcaebf4af9a8ffc2c832aef6978
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-09-20 01:05:59 +0530

    delete product api

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -54,8 +54,23 @@ export const updateProduct = async (req,res) => {
         }
 
         const updatedProduct = await Product.findByIdAndUpdate(id, {name,price,image}, {new:true});
-        res.status(200).json({message:true, message:`product data updated`, updatedProduct})
+        res.status(200).json({success:true, message:`product data updated`, updatedProduct})
     } catch (error) {
         res.status(500).json({message:"internal server error"});
+    }
+}
+
+export const deleteProduct = async (req,res) => {
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({success:false,message:`Invalid Id`});
+    }
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success:true,message:"product deleted"});
+    } catch (error) {
+        res.status(500).json({success:false,message:"internal serer error"});
     }
 }
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getProducts, createProduct, updateProduct } from "../controllers/product.controller.js";
+import { getProducts, createProduct, updateProduct, deleteProduct } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 router.get("/",getProducts);
 router.post("/createProduct", createProduct);
 router.put("/updateProduct/:id", updateProduct);
+router.delete("/deleteProduct/:id", deleteProduct);
 
 
 export default router;

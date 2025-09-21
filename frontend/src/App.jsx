f83814bfb783874ece7ca265af4132852726e967
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-09-22 00:45:28 +0530

    Create Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Create from './pages/Create'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/add" element={<Create/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/Create.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import axios from "axios";
+const Create = () => {
+
+    const[name,setName] = useState("");
+    const[price,setPrice] = useState("");
+    const[imageUrl,setImageUrl] = useState("");
+    const[loading,setLoading] = useState(false)
+
+    const createProduct = async () => {
+        try {
+                setLoading(true);
+                const res = await axios.post(`${import.meta.env.VITE_API_URL}/createProduct`, {name,price,image:imageUrl})
+
+                if(res.data.success) {
+                    alert("Prodcut Create Successfully");
+                    setName("");
+                    setPrice("");
+                    setImageUrl("");
+                } else {
+                    alert("Failed to create product");
+                }
+        } catch (error) {
+                console.error(error);
+                alert("Server Error")
+        } finally {
+            setLoading(false);        
+        }
+    }
+
+
+  return (
+    <div className='flex justify-center items-center h-screen bg-gradient-to-r from-violet-800 to-cyan-900 text-white'>
+      <div className='bg-cyan-900 p-6 w-96 shadow-md rounded-md'>
+        <p className='text-3xl font-bold mb-4 text-center'>Add a Product</p>
+        <div className='flex flex-col space-y-3 w-full'>
+          <input
+            onChange={(e)=>{setName(e.target.value)}}
+            value={name}
+           className='p-4 text-xl w-full rounded-md' type="text" placeholder='Enter name of product'/>
+          <input
+            onChange={(e)=>{setPrice(e.target.value)}}
+            value={price}
+           className='p-4 text-xl w-full rounded-md' type="text" placeholder='Enter price of product'/>
+          <input 
+            onChange={(e)=>{setImageUrl(e.target.value)}}
+            value={imageUrl}
+          className='p-4 text-xl w-full rounded-md' type="text" placeholder='Enter image url of product'/>
+          <button disabled={loading} onClick={createProduct} className='mt-5 px-4 py-2 cursor-pointer bg-violet-400 w-full rounded-lg text-sm font-bold hover:bg-violet-500 transition-colors duration-300'>
+            Add the Product
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Create

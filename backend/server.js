6729f97c3ce06ff21294613afc6d4bd424a45db9
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-10-28 01:15:21 +0530

    finish

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import cors from "cors";
+import path from "path";
 dotenv.config();
 
 import productRoutes from "./routes/product.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 
@@ -22,6 +25,14 @@ app.get("/", (req,res) => {
 })
 
 app.use("/api/products", productRoutes)
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname , "/frontend/dist")));
+
+  app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  })
+}
 
 app.listen(PORT, () => {
     connectDB();
